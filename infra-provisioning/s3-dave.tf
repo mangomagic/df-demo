df8f386cd33a9117435c2780f8997e0342243e0c
mangomagic <rob.barty@gmail.com>
2025-03-31 08:36:27 +0100

    Remove code incompatible with localstack

--- infra-provisioning/s3-dave.tf
+++ infra-provisioning/s3-dave.tf
@@ -12,34 +12,6 @@ resource "aws_s3_bucket_versioning" "dave_versioning" {
   }
 }
 
-#
-# Code below would create a KMS key and apply to bucket
-# Currently not working with localstack
-#
-
-# Create a KMS Key
-# resource "aws_kms_key" "dave_kms_key" {
-#   description             = "KMS key for Dave's S3 bucket"
-#   key_usage               = "ENCRYPT_DECRYPT"
-#   deletion_window_in_days = 10
-
-#   tags = {
-#     Name        = "Dave KMS Key"
-#     Environment = "Test"
-#   }
-# }
-
-# S3 bucket encryption configuration
-# resource "aws_s3_bucket_server_side_encryption_configuration" "dave_encryption" {
-#   bucket = aws_s3_bucket.dave.id
-#   rule {
-#     apply_server_side_encryption_by_default {
-#       sse_algorithm     = "aws:kms"
-#       kms_master_key_id = aws_kms_key.dave_kms_key.id # Use the KMS key
-#     }
-#   }
-# }
-
 # AWS Server Side Encryption
 resource "aws_s3_bucket_server_side_encryption_configuration" "dave_encryption" {
   bucket = aws_s3_bucket.dave.id
